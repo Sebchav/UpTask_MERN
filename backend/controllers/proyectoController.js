29c57e84337bb8ca96d856dce5f75c8791f6fa0b
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-07-16 22:42:00 -0600

    Create new Projects

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -1,7 +1,9 @@
 import Proyecto from "../models/Proyecto.js";
+import mongoose from "mongoose"; 
 
 const obtenerProyectos = async (req, res)=>{
-
+    const proyectos = await Proyecto.find().where("creador").equals(req.usuario);
+    res.json(proyectos);
 };
 
 const nuevoProyecto = async(req, res)=>{
@@ -17,14 +19,74 @@ const nuevoProyecto = async(req, res)=>{
 };
 
 const obtenerProyecto = async(req, res)=>{
-
+    const { id } = req.params;
+    let proyecto;
+    
+    if(mongoose.Types.ObjectId.isValid(id)){
+      proyecto = await Proyecto.findById(id);
+    } else { 
+      const error = new Error('El proyecto no fue encontrado');
+      return res.status(404).json({ msg: error.message});
+    }
+    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+      const error = new Error('Acción no valida');
+      return res.status(401).json({msg: error.message})
+    }
+    res.json(proyecto);
+    
 };
 
 const editarProyecto = async(req, res)=>{
+    const { id } = req.params;
+    let proyecto;
+    
+    if(mongoose.Types.ObjectId.isValid(id)){
+      proyecto = await Proyecto.findById(id);
+    } else { 
+      const error = new Error('El proyecto no fue encontrado');
+      return res.status(404).json({ msg: error.message});
+    }
 
+    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+      const error = new Error('Acción no valida');
+      return res.status(401).json({msg: error.message})
+    }
+
+    proyecto.nombre = req.body.nombre || proyecto.nombre;
+    proyecto.descripcion = req.body.descripcion || proyecto.descripcion
+    proyecto.fechaEntrega = req.body.fechaEntrega || proyecto.fechaEntrega
+    proyecto.cliente = req.body.cliente || proyecto.cliente
+
+    try{
+        const proyectoAlmacenado = await proyecto.save();
+        res.json(proyectoAlmacenado);
+    }catch(error){
+        console.log(error)
+    }
 };
 
 const eliminarProyecto = async(req, res)=>{
+    const { id } = req.params;
+    let proyecto;
+    
+    if(mongoose.Types.ObjectId.isValid(id)){
+      proyecto = await Proyecto.findById(id);
+    } else { 
+      const error = new Error('El proyecto no fue encontrado');
+      return res.status(404).json({ msg: error.message});
+    }
+
+    if(proyecto.creador.toString() !== req.usuario._id.toString()){
+      const error = new Error('Acción no valida');
+      return res.status(401).json({msg: error.message})
+    }
+
+    try{
+        await proyecto.deleteOne();
+        res.json({msg: "Proyecto Eliminado"});
+    }catch(error){
+        console.log(error)
+    }
 
 };
 
