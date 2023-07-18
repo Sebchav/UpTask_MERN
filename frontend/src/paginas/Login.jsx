3671a6dc8d679a0da54bc95690267984e8bd4b84
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-07-17 22:09:07 -0600

    Beginning with the Front: Installing Tailwind, Creating Routing with React Router DOM, principal layout, creating Auth Form (login) and Adding routing to other public pages

---
+++ frontend/src/paginas/Login.jsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom"
+
+const Login = () => {
+  return (
+    <>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">
+            Inicia Sesión y administra tus 
+            <span className="text-slate-700"> proyectos</span>
+        </h1>
+
+        <form className="my-10 bg-white shadow rounded-lg p-10">
+            <div className="my-5">
+                <label 
+                    className="uppercase text-gray-600 block text-xl font-bold"
+                    htmlFor="email"
+                    >Email
+                </label>
+                <input 
+                    id="email"
+                    type="email"
+                    placeholder="Email de registro"
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                />
+            </div>
+
+            <div className="my-5">
+                <label 
+                    className="uppercase text-gray-600 block text-xl font-bold"
+                    htmlFor="password"
+                    >Password
+                </label>
+                <input 
+                    id="password"
+                    type="password"
+                    placeholder="Password de registro"
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                />
+            </div>
+
+            <input
+                type="submit"
+                value="Iniciar Sesión"
+                className="bg-sky-700 w-full py-3 text-white uppercase font-bold rounded hover:cursor-pointer hover:bg-sky-800 transition-colors mb-5"
+            />
+        </form>
+
+        <nav className="lg:flex lg:justify-between">
+            <Link
+                to="/registrar"
+                className="block text-center my-5 text-slate-500 uppercase text-sm"
+            >
+                ¿No tienes una cuenta? Registrate
+            </Link>
+
+            <Link
+                to="/olvide-password"
+                className="block text-center my-5 text-slate-500 uppercase text-sm"
+            >
+                Olvidé mi password
+            </Link>
+        </nav>
+    </>
+  )
+}
+
+export default Login
